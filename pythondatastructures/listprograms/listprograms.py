51683b6c4322c812ecf1a9e7ed925f63ff55ea5a
Anjana <anjanamg97@gmail.com>
2020-11-25 09:16:22 +0530

    25/11/2020

---
+++ pythondatastructures/listprograms/listprograms.py
@@ -0,0 +1,9 @@
+list=["java","python","c#","javascript"]
+# print(type(list))
+# print(list[-1:-4:-1])
+# list.append("dart")
+# list[1]="ruby"
+# for item in list:
+#     # print(item)
+list.insert(2,"c")
+print(list)
